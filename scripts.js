361937cec2290b85b563a83c31fa5ed70c529afa
FBasualdo <127265422+FBasualdo@users.noreply.github.com>
2023-04-25 00:59:58 -0300

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,21 @@
+/*function ampliarProyecto(element) {
+  // Obtener todos los elementos de proyecto
+  const proyectos = document.querySelectorAll('.project-div');
+  element.classList.add('expands')
+    // Quitar la clase ampliado de los otros proyectos
+  proyectos.forEach((proyecto) => {
+    if (proyecto !== element) {
+      proyecto.classList.remove('expands');
+      proyecto.classList.add('rest')
+    }
+  });
+}
+
+function mouseout() {
+const proyectos = document.querySelectorAll('.project-div');
+proyectos.forEach((proyecto) => {
+  proyecto.classList.remove('expands');
+  proyecto.classList.remove('rest')
+  });
+}
+*/
